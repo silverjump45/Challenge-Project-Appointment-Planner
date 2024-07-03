dc70ed6c3637c619dafbac7f237f9fa8895192ef
silverjump45 <waltleinen86@gmail.com>
2024-07-02 21:09:04 -0500

    1st draft with PropTypes

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,21 @@
+import PropTypes from "prop-types";
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({name, description}) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
+      {Object.keys(description).map((key, index) => (
+        <p key={key + index} className="tile">
+          {description[key]}
+        </p>
+      ))}
+
     </div>
   );
+};
+
+Tile.propTypes ={
+  name: PropTypes.string.isRequired,
+  description: PropTypes.object.isRequired,
 };
