e55bda84d3dfa3f96d3d73d10fe1ab3dd6385da3
silverjump45 <waltleinen86@gmail.com>
2024-07-02 21:42:19 -0500

    1st draft

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,31 +1,62 @@
 import React, { useState } from "react";
-
+import PropTypes from "prop-types";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({ appointsments, contacts, addAppointment }) => {
   /*
   Define state variables for 
   appointment info
   */
+  const [title, setTitle] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+    if(!contact) return;
+    addAppointment(title, contact, date, time);
+    clearForm();
+  };
+
+  const clearForm =() => {
+    setTitle("");
+    setContact("");
+    setDate("");
+    setTime("");
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          title={title}
+          contact={contact}
+          contacts={contacts}
+          date={date}
+          time={time}
+          setTitle={setTitle}
+          setContact={setContact}
+          setDate={setDate}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList appointmentsOrContacts={appointsments} />
       </section>
     </div>
   );
+};
+
+AppointmentsPage.propTypes ={
+  appointsments: PropTypes.array.isRequired,
+  contacts: PropTypes.array.isRequired,
+  addAppointment: PropTypes.func.isRequired,
 };
